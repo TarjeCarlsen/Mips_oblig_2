25996baae99b8a96ba8e99b651351758cbfb6a15
Tristan Natvig <tristan.natvig@gmail.com>
2025-10-09 01:05:16 +0200

    Dekoder fungerer

--- src/simulator.py
+++ src/simulator.py
@@ -43,29 +43,29 @@ def decoder(pc_instructions):
 def runSimulator(sim):
     # Replace this with your own main loop!
     counter = 0
-    while (counter < 25):
+    while (counter < 4):
         sim.tick()
         # print(sim.dataMemory.memory)
         pc_val = sim.pc.currentAddress()
-        memory = sim.instructionMemory.memory
-        instr = sim.instructionMemory.memory.get(pc_val,0)
-
         converted_adress = (pc_val - (pc_val % 4))
+        memory = sim.instructionMemory.memory
+        instr = sim.instructionMemory.memory.get(converted_adress,0)
+
         # print(f"PC={converted_adress}  Instr={hex(instr)}")
         # print(sim.pc.currentAddress())
         count = 0
         
-        for key,value in memory.items():
-            if(key == converted_adress):
-                print(f"count {count} key - {key} adress - {converted_adress}")
-        
-                print(f"{hex(key)} - {hex(value)}")
-            count = count +1
-
-        if(converted_adress in memory):
-             print(f"pc value = {hex(pc_val)} found in memory")
-        else:
-             print(f"pc value = {hex(pc_val)} not found in memory")
+        #for key,value in memory.items():
+        #    if(key == converted_adress):
+        #        print(f"count {count} key - {key} adress - {converted_adress}")
+        #
+        #        print(f"{hex(key)} - {hex(value)}")
+        #    count = count +1
+#
+        #if(converted_adress in memory):
+        #     print(f"pc value = {hex(pc_val)} found in memory")
+        #else:
+        #     print(f"pc value = {hex(pc_val)} not found in memory")
 
         pc_value = sim.pc.getOutputValue("pcAddress")
 
